ecbb3184caf9199e888e44256d0df0c7c7922b3d
bobur <uboiligarh@gmail.com>
2022-11-26 11:11:02 -0500

    added models

---
+++ database/models/reqresin_models.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+
+engine = create_engine('sqlite:///users.db', echo=True)
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user_table"
+    id = Column(Integer, primary_key=True)
+    email = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    avatar = Column(String)
+    address = relationship("Address", uselist=False, backref=backref("user_table"))
+
+
+class Address(Base):
+    __tablename__ = "address_table"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user_table.id"))
+    address = Column(String)
+
+
+class Contact(Base):
+    __tablename__ = "contact_table"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user_table.id"))
+    phone_number = Column(String)
+
+
