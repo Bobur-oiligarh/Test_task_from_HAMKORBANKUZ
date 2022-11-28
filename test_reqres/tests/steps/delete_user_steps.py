413b8119b3be068e831b8defb0181ae8f38348ad
bobur <uboiligarh@gmail.com>
2022-11-28 08:45:46 -0500

    cosmetic change

--- test_reqres/tests/steps/delete_user_steps.py
+++ test_reqres/tests/steps/delete_user_steps.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from test_reqres.http_requests.delete_user import DeleteUser
-
+from requests.status_codes import codes
 import allure
 
 
@@ -8,5 +8,5 @@ import allure
 def step_delete_user():
     response = DeleteUser().response()
     status_code = response.status_code
-    TestCase().assertEqual(status_code, 204)
+    TestCase().assertEqual(status_code, codes.no_content)
 
